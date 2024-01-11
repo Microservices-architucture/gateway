d040b9a46054a5dc7703e8fc2b62b2faf31b8b98
imen lakrib <lak12imen@gmail.com>
2024-01-11 14:35:03 +0100

    old code

--- index.ts
+++ index.ts
@@ -5,86 +5,32 @@ import { ApolloServer } from "apollo-server-express";
 import { ExpressContext } from "apollo-server-express";
 import { ApolloGateway, IntrospectAndCompose } from "@apollo/gateway";
 import jwt from "jsonwebtoken";
+import { Console } from "console";
 
 dotenv.config();
-
-const context = ({ req }: ExpressContext) => {
-  const authorizationHeader = req.headers.authorization || "";
-  const [bearer, token] = authorizationHeader.split(" ");
-
-  if (bearer && bearer.toLowerCase() === "bearer" && token) {
-    const user = extractUserFromToken(token);
-    console.log("Gateway context:", { user });
-    return { user };
-  } else {
-    console.error("Invalid authorization header format");
-    return { user: null };
-  }
-};
-
-const extractUserFromToken = (token: string) => {
-  try {
-    console.log("Token value:", token); // Log the token
-    const decodedToken: any = jwt.verify(
-      token,
-      process.env.SECRET_KEY || "8000",
-      { ignoreExpiration: false } // Ensure token is not expired
-    );
-    console.log("Decoded token:", decodedToken);
-    return {
-      userId: decodedToken.userId,
-    };
-  } catch (error) {
-    console.error(
-      "Error verifying token:",
-      (error as { message: string }).message
-    );
-    return null;
-  }
-};
 
 const gateway = new ApolloGateway({
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
       {
         name: "auth",
-        url: "http://localhost:4001/graphql",
+        url: "http://localhost:5000/graphql",
       },
       {
         name: "posts",
         url: "http://localhost:7006/graphql",
       },
     ],
-
-    introspectionHeaders: {
-      originalAuthorization: "Bearer abc123",
-    },
   }),
 });
 
 const server = new ApolloServer({
   gateway,
   context: ({ req }: ExpressContext) => {
-    console.log("Headers in posts service:", req.headers);
-
     const authorizationHeader = req.headers.authorization || "";
-    console.log("Gateway context:", { authorizationHeader });
-
-    const [bearer, token] = authorizationHeader.split(" ");
-
-    if (bearer && bearer.toLowerCase() === "bearer" && token) {
-      const user = extractUserFromToken(token);
-      console.log("Gateway context:", { user });
-      return {
-        user,
-        headers: {
-          authorization: token,
-        },
-      };
-    } else {
-      console.error("Invalid authorization header format");
-      return { user: null };
-    }
+    const token = authorizationHeader.split(" ")[1] || "";
+    console.log("CONTEXT GATEWAY TOKEN VALUE", token);
+    return { token };
   },
 });
 
